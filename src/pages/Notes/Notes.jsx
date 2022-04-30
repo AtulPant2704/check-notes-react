e45778f9235175592f6beb8f26cc26ef8127b148
AtulPant2704 <atulpant1999@gmail.com>
2022-05-01 04:56:58 +0530

    refactor: made Filter as separate component

--- src/pages/Notes/Notes.jsx
+++ src/pages/Notes/Notes.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { SideBar, Note, NoteModal } from "../../components";
+import { Note, NoteModal } from "../../components";
+import { Filter } from "./components/Filter";
 import "./Notes.css";
 
 const Notes = () => {
@@ -35,34 +36,7 @@ const Notes = () => {
             showFilters ? "filter-container-active" : ""
           }`}
         >
-          <div className="filter-header">
-            <h2 className="filter-heading">Sort</h2>
-            <button className="btn btn-solid-primary filter-clear-btn">
-              Clear
-            </button>
-          </div>
-          <div className="filter">
-            <h3 className="filter-type">By Time:</h3>
-            <div className="filter-type-container">
-              <input type="radio" id="new-to-old" name="time-filter" />
-              <label for="new-to-old">New to Old</label>
-            </div>
-            <div className="filter-type-container">
-              <input type="radio" id="old-to-new" name="time-filter" />
-              <label for="old-to-new">Old to New</label>
-            </div>
-          </div>
-          <div className="filter">
-            <h3 className="filter-type">By Priority:</h3>
-            <div className="filter-type-container">
-              <input type="radio" id="high-to-low" name="priority-filter" />
-              <label for="high-to-low">High to Low</label>
-            </div>
-            <div className="filter-type-container">
-              <input type="radio" id="low-to-high" name="priority-filter" />
-              <label for="low-to-high">Low to High</label>
-            </div>
-          </div>
+          <Filter />
         </div>
 
         <section className="add-note-section">
