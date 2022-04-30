f8391ce402521b31234dc229e9fd7fa4bc15cbc0
AtulPant2704 <atulpant1999@gmail.com>
2022-05-01 01:44:46 +0530

    feat: add note card

---
+++ src/components/Note/Note.jsx
@@ -0,0 +1,29 @@
+import "./Note.css";
+
+const Note = () => {
+  return (
+    <div className="note">
+      <button className="pin-btn">
+        <span class="material-icons-outlined">push_pin</span>
+      </button>
+      <h2>Title</h2>
+      <h4>Content</h4>
+      <div className="note-footer">
+        <p className="note-date">02/04/2020 05:20</p>
+        <div className="note-action-btns">
+          <button className="action-btn">
+            <span class="material-icons-outlined nav-icon">delete_outline</span>
+          </button>
+          <button className="action-btn">
+            <span class="material-icons-outlined nav-icon">archive</span>
+          </button>
+          <button className="action-btn">
+            <span class="material-icons-outlined nav-icon">edit</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export { Note };
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { LoginModal } from "./Authentication/LoginModal";
 export { SignupModal } from "./Authentication/SignupModal";
 export { SideBar } from "./Sidebar/Sidebar";
+export { Note } from "./Note/Note";
