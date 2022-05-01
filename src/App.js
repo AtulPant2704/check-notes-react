0a938aca06e321aeec563008793335906f867422
AtulPant2704 <atulpant1999@gmail.com>
2022-05-01 15:55:24 +0530

    feat: made routes protected

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Routes, Route, useLocation } from "react-router-dom";
+import { RequiresAuth } from "./RequiresAuth";
 import { SideBar } from "./components";
 import { Landing, Trash, Archive, Notes, Label } from "./pages";
 import "./App.css";
@@ -11,10 +12,38 @@ function App() {
       {location.pathname !== "/" ? <SideBar /> : null}
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/notes" element={<Notes />} />
-        <Route path="/label" element={<Label />} />
-        <Route path="/archive" element={<Archive />} />
-        <Route path="/trash" element={<Trash />} />
+        <Route
+          path="/notes"
+          element={
+            <RequiresAuth>
+              <Notes />
+            </RequiresAuth>
+          }
+        />
+        <Route
+          path="/label"
+          element={
+            <RequiresAuth>
+              <Label />
+            </RequiresAuth>
+          }
+        />
+        <Route
+          path="/archive"
+          element={
+            <RequiresAuth>
+              <Archive />
+            </RequiresAuth>
+          }
+        />
+        <Route
+          path="/trash"
+          element={
+            <RequiresAuth>
+              <Trash />
+            </RequiresAuth>
+          }
+        />
       </Routes>
     </div>
   );
