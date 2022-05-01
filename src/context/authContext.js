ce072952dc72a15b69baf083167e90cd3acb8af0
AtulPant2704 <atulpant1999@gmail.com>
2022-05-01 15:45:11 +0530

    feat: add auth context and reducer

---
+++ src/context/authContext.js
@@ -0,0 +1,31 @@
+import { createContext, useContext, useReducer } from "react";
+import { authReducer } from "../reducer/auth-reducer";
+
+const initialState = {
+  user: "",
+  token: "",
+};
+
+const AuthContext = createContext(null);
+
+const AuthProvider = ({ children }) => {
+  const [authState, authDispatch] = useReducer(authReducer, initialState);
+
+  if (!authState.token) {
+    const token = localStorage.getItem("token");
+    const user = JSON.parse(localStorage.getItem("user"));
+    token && user
+      ? authDispatch({ type: "CHECK_USER", payload: { user, token } })
+      : null;
+  }
+
+  return (
+    <AuthContext.Provider value={{ authState, authDispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+const useAuth = () => useContext(AuthContext);
+
+export { AuthProvider, useAuth };
--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import { makeServer } from "./server";
+import { AuthProvider } from "./context";
 
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <AuthProvider>
+      <Router>
+        <App />
+      </Router>
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
