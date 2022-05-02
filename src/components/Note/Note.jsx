9c837b1702d04288d04d86e04f818d01db1ba46d
AtulPant2704 <atulpant1999@gmail.com>
2022-05-03 02:41:40 +0530

    feat: add, restore and display notes

--- src/components/Note/Note.jsx
+++ src/components/Note/Note.jsx
@@ -1,7 +1,30 @@
+import { useLocation } from "react-router-dom";
 import ReactHtmlParser from "react-html-parser";
+import { useAuth, useNotes, useArchive } from "../../context";
+import {
+  addNoteToArchiveHandler,
+  restoreNoteFromArchiveHandler,
+} from "../../utils";
 import "./Note.css";
 
 const Note = ({ note, setShowNoteModal, setEditNote }) => {
+  const { pathname } = useLocation();
+  const {
+    authState: { token },
+  } = useAuth();
+  const { notesDispatch } = useNotes();
+  const { archiveDispatch } = useArchive();
+
+  const addNoteToArchive = (e) => {
+    e.stopPropagation();
+    addNoteToArchiveHandler(token, note, archiveDispatch, notesDispatch);
+  };
+
+  const restoreNoteFromArchive = (e) => {
+    e.stopPropagation();
+    restoreNoteFromArchiveHandler(token, note, archiveDispatch, notesDispatch);
+  };
+
   const editNoteHandler = () => {
     setEditNote(note);
     setShowNoteModal(true);
@@ -22,12 +45,21 @@ const Note = ({ note, setShowNoteModal, setEditNote }) => {
               delete_outline
             </span>
           </button>
-          <button className="action-btn">
+          <button
+            className="action-btn"
+            onClick={
+              pathname !== "/archive"
+                ? addNoteToArchive
+                : restoreNoteFromArchive
+            }
+          >
             <span className="material-icons-outlined nav-icon">archive</span>
           </button>
-          <button className="action-btn" onClick={editNoteHandler}>
-            <span className="material-icons-outlined nav-icon">edit</span>
-          </button>
+          {pathname !== "/archive" ? (
+            <button className="action-btn" onClick={editNoteHandler}>
+              <span className="material-icons-outlined nav-icon">edit</span>
+            </button>
+          ) : null}
         </div>
       </div>
     </div>
