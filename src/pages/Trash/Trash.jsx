4282561216d65d8d12d03bca5b6ff1eb01086473
AtulPant2704 <atulpant1999@gmail.com>
2022-05-03 12:08:44 +0530

    feat: display trash notes

--- src/pages/Trash/Trash.jsx
+++ src/pages/Trash/Trash.jsx
@@ -1,16 +1,32 @@
+import { useEffect } from "react";
+import { useAuth, useTrash } from "../../context";
+import { getTrashHandler } from "../../utils";
 import { Note } from "../../components";
 import "./Trash.css";
 
 const Trash = () => {
+  const {
+    authState: { token },
+  } = useAuth();
+  const {
+    trashState: { trash },
+    trashDispatch,
+  } = useTrash();
+
+  useEffect(() => {
+    getTrashHandler(token, trashDispatch);
+  }, []);
+
+  console.log(trash);
+
   return (
     <>
       <main className="delete-page">
         <h2 className="page-title">Deleted Notes</h2>
         <section className="deleted-notes-container">
-          <Note />
-          <Note />
-          <Note />
-          <Note />
+          {trash.map((note) => (
+            <Note key={note._id} note={note} />
+          ))}
         </section>
       </main>
     </>
