cca5c28bf5102d7acca13b16711e336863b40454
AtulPant2704 <atulpant1999@gmail.com>
2022-05-20 00:09:37 +0530

    feat: add images by colors

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,15 @@
-import landingImage from "./Work time-amico.webp";
 import image404 from "./404-error-img.webp";
+import blueLanding from "./blue-landing.webp";
+import greenLanding from "./green-landing.webp";
+import purpleLanding from "./purple-landing.webp";
+import redLanding from "./red-landing.webp";
+import yellowLanding from "./yellow-landing.webp";
 
-export { landingImage, image404 };
+export {
+  image404,
+  blueLanding,
+  greenLanding,
+  purpleLanding,
+  redLanding,
+  yellowLanding,
+};
